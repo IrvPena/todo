862c28b6c357039faabf0a21f8276dee47adc23e
IrvPena <one.irv@gmail.com>
2020-07-26 05:37:22 -0400

    Updates

---
+++ precache-manifest.240d69586cee33d5d764bbac35a7efdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b007953eafbd3520b09a11165ccca8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "de315867a9154b01f2f3",
+    "url": "/todo/static/css/main.e1ac7eef.chunk.css"
+  },
+  {
+    "revision": "653081b9d37e49d0bf28",
+    "url": "/todo/static/js/2.4da34c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4da34c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de315867a9154b01f2f3",
+    "url": "/todo/static/js/main.aae67606.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
